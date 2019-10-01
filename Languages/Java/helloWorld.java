2abc79847b9e071cba321b655e186236f3bfcb05
shanky199912 <shanky199912@outlook.com>
2019-10-01 10:31:07 +0530

    created hello_world.java

--- Languages/Java/helloWorld.java
+++ Languages/Java/helloWorld.java
@@ -2,6 +2,6 @@ import java.lang.*;
 
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
