3b1f9b297e97ad8c0f9fbd53439ac4769e12ddac
tejas10p <56042232+tejas10p@users.noreply.github.com>
2019-10-02 01:45:32 +0530

    Largest_Sum_Continguous_Subarray_upd.cpp

--- Languages/C++/Largest_Sum_Contiguous_Subarray.cpp
+++ Languages/C++/Largest_Sum_Contiguous_Subarray.cpp
@@ -19,12 +19,26 @@ int maxSubArraySum(int a[], int size)
 	return max_so_far; 
 } 
 
+int maxSum_minSum(int arr[],int size){ // To find maximum continguous subarray sum using minimum subarray sum till an index in O(N)
+	int min_sum = 0, max_sum = INT_MIN, cur = 0;
+	
+    	for(int i = 0;i < size;i++){
+        	cur += arr[i];
+        	max_sum = max(max_sum,cur - min_sum);
+        	min_sum = min(cur,min_sum);
+    	}
+	
+    	return max_sum;
+}
+
 /*Driver program to test maxSubArraySum*/
 int main() 
 { 
 	int a[] = {-2, -3, 4, -1, -2, 1, 5, -3}; 
 	int n = sizeof(a)/sizeof(a[0]); 
 	int max_sum = maxSubArraySum(a, n); 
-	cout << "Maximum contiguous sum is " << max_sum; 
+	int max_sum2 = maxSum_minSum(a, n);
+	cout << "Maximum contiguous sum is " << max_sum;
+	cout << "\nMaximum continguous sum found using minimum sum is " << max_sum2;
 	return 0; 
 } 
