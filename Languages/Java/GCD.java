328bb159af18137d0b4fce9d7effe6a97e44edfa
shanky199912 <shanky199912@outlook.com>
2019-10-01 11:45:59 +0530

    created GCD.java

---
+++ Languages/Java/GCD.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class GCD {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        int divident = scn.nextInt();
+        int divisor = scn.nextInt();
+
+        while (divident % divisor != 0) {
+
+            int rem = divident % divisor;
+
+            divident = divisor;
+            divisor = rem;
+
+        }
+
+        System.out.println(divisor);
+
+    }
+}
