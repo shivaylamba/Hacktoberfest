d08aba89b516a8f14f9e2f17e3888355897ced9a
DeltaCoderr <51528076+DeltaCoderr@users.noreply.github.com>
2019-10-04 12:55:38 +0530

    Update js-hello-world.js
    grammar.

--- Languages/Javascript/js-hello-world.js
+++ Languages/Javascript/js-hello-world.js
@@ -1,5 +1,5 @@
 console.log("This is going to be my first Open Source Contribution.");
-console.log("THus, all that I would want to say is that, this is a new phase I am getting into.");
+console.log("Thus, all that I would want to say is that, this is a new phase I am getting into.");
 console.log("And to everyone who is reading this, ");
 console.log("I wish you all a very warm and heartfull: ");
 
