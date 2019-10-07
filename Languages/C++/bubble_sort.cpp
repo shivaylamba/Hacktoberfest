19e770f507269facf902a56c0a6cb5fcc9ec2d1f
nafees87n <56021937+nafees87n@users.noreply.github.com>
2019-10-07 12:28:36 +0530

    Added Bubble Sort.

---
+++ Languages/C++/bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void sort(int a[],int size)
+{
+	for(int i=0;i<size-1;i++)
+	{
+		for(int j=0;j<size-1-i;j++)
+			{
+				if(a[j]>=a[j+1])
+				{
+					int t=a[j];
+					a[j]=a[j+1];
+					a[j+1]=t;
+				}
+			}
+	}
+}
+int main()
+{
+	int n;
+	cout<<"Enter Size of Array:";
+	cin>>n;
+	int a[n];
+	cout<<"Enter Elements:";
+	for(int p=0;p<n;p++)
+		cin>>a[p];
+	sort(a,n);
+	for(int i=0;i<n;i++)
+		cout<<a[i]<<"  ";
+	return 0;
+}
