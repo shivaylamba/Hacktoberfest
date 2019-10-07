00b32f4dd742351d2c7f7b6cc3ee69eaccbe4082
nitishp25 <pasrichanitish2@gmail.com>
2019-10-07 12:44:44 +0530

    removed unnecessary variable

--- Languages/C++/palindrome.cpp
+++ Languages/C++/palindrome.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main() {
 
     string s;
-    int t = 1, n;
+    int t = 1;
+
+    // input
     cout<<"Enter string: ";
     getline(cin, s);
 
+    // iterators for reading string
     string::iterator it, it1;
     it1 = s.end() - 1;
 
+    // reading and comparing from start and end
     for(it = s.begin(); it != s.end(); it++) {      
         if(*it != *it1) {
             t = 0;
@@ -20,6 +24,7 @@ int main() {
         it1--;
     }
 
+    // finally checking
     if(t == 1)
         cout<<"Palindrome"<<endl;
     else
