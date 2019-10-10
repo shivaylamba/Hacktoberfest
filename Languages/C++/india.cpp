951af6b5baf65080b6bbe472cea68536fd88c85c
bestin <bestinbthomas@gmail.com>
2019-10-10 21:43:40 +0530

    [ADD] india map

---
+++ Languages/C++/india.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    for (b=c=10; a="- FIGURE?, UMKC,XYZHello Folks,\TFy!QJu ROo TNn(ROo)SLq SLq ULo+UHs UJq TNn*RPn/QPbEWS_JSWQAIJO^\NBELPeHBFHT}TnALVlBLOFAkHFOuFETp\HCStHAUFAgcEAelclcn^r^r\\tZvYxXy\T|S~Pn SPm SOn TNn ULo0ULo#ULo-W\Hq!WFs XDt!"  [b+++21]; )
+    for(; a-- > 64 ; ) putchar ( ++c=='Z' ? c = c/ 9:33^b&1);
+    printf(" 🇮🇳 ❤ #LoveYouIndia, #PatrioticCoders Download and Share source code. To generate Map of India🇮🇳 with just 15 lines of code in C++ in just 2 minutes of time download codingclub.tech/static/myindia.txt  Jai Hind 🇮🇳") ;
+    //Step 1: Compile and run it.
+    //Step 2: Capture screenshot of India Map
+    //Step 3: Copy string between printf brackets line no.8 and paste it on Linkedin with screenshot of your output. Happy Coding ! Jai Hind !🇮🇳
+    //Original Code credits : IOCCC.
+    //This code was modified and written on 14th August at 20:03pm, 2019 at India #LearnInspireGrow 🇮🇳
+	return 0;
+}
