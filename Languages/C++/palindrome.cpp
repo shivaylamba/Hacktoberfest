f582cf72a7f0f0ae87a476c17fa8eca2c6eff3a1
Sakshi Gupta <itzsakshi26gupta@gmail.com>
2019-10-11 20:31:37 +0530

    changed variable names to relevent names

--- Languages/C++/palindrome.cpp
+++ Languages/C++/palindrome.cpp
@@ -4,28 +4,28 @@ using namespace std;
 
 int main() {
 
-    string s;
-    int t = 1;
+    string str;
+    int count = 1;
 
     // input
     cout<<"Enter string: ";
-    getline(cin, s);
+    getline(cin, str);
 
     // iterators for reading string
-    string::iterator it, it1;
-    it1 = s.end() - 1;
+    string::iterator i, j;
+    j = str.end() - 1;
 
     // reading and comparing from start and end
-    for(it = s.begin(); it != s.end(); it++) {      
-        if(*it != *it1) {
-            t = 0;
+    for(i = str.begin(); i!= str.end(); i++) {      
+        if(*i != *j) {
+            count = 0;
             break;
         }
-        it1--;
+        j--;
     }
 
     // finally checking
-    if(t == 1)
+    if(count == 1)
         cout<<"Palindrome"<<endl;
     else
         cout<<"Not Palindrome"<<endl;
