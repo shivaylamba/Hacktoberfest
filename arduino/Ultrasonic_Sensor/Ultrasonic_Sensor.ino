db6c771c1cd12a30e7bce21d1d322314c84f6cfc
Rajesh Hegde <33092803+irajeshh@users.noreply.github.com>
2019-10-15 07:27:18 +0530

    arduino files

---
+++ arduino/Ultrasonic_Sensor/Ultrasonic_Sensor.ino
@@ -0,0 +1,23 @@
+#define tp 3
+#define ep 2
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(tp,OUTPUT);
+  pinMode(ep, INPUT);
+}
+void loop()
+{
+  int time;
+  float dist;
+  digitalWrite(tp,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(tp,LOW);
+  time = pulseIn(ep,HIGH);
+  dist = (time/2)/29.1;
+  
+  Serial.print(dist);
+  Serial.println("m");
+  delay(500);
+}
+  
