6976645b022ea2b9ea56b9405cee3e80df8319ce
PRIYANSHU RANJAN <priyanshuranjan2001@gmail.com>
2020-10-01 16:58:51 +0530

    SWAPING WITHOUT 3RD VARIABLE

---
+++ Swaping.py
@@ -0,0 +1,7 @@
+a=int(input("enter number"))
+b=int(input("enter number"))
+print("number after swapping are a = ",a," & b = ",b)
+a=a+b
+b=a-b
+a=a-b
+print("number after swapping are a = ",a," & b = ",b)
