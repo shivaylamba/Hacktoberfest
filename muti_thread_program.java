17f09c1e2af1fe7f606b7744513522e7aa5e92b4
Archit099 <65014238+Archit099@users.noreply.github.com>
2020-10-03 11:57:36 -0600

    Update muti_thread_program.java

--- muti_thread_program.java
+++ muti_thread_program.java
@@ -1,10 +1,5 @@
-// Name:- Sorathiya Archit
-// Enrollment no.:- 181310132054
-// Problem is
-/*Write an application that creates and starts three threads. Each thread is instantiated
-from the same class. It executes a loop with 10 iterations. Each iteration displays string
-"HELLO", sleeps for 300 milliseconds. The application waits for all the threads to
-complete & displays the message "Good Bye...".*/
+
+
 
 package oop;
 
