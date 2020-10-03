04d0033d6a558d7fbba624151dd2fd9f3142ed9a
kmuteen <72196830+kmuteen@users.noreply.github.com>
2020-10-03 21:02:39 +0530

    Add files via upload

--- Magic 8-Ball.py
+++ Magic 8-Ball.py
@@ -5,7 +5,7 @@ import random
 ans = True
 
 while ans:
-    question = raw_input("Ask the magic 8 ball a question: (press enter to quit) ")
+    question = input("Ask the magic 8 ball a question: (press enter to quit) ")
     
     answers = random.randint(1,8)
     
@@ -13,25 +13,25 @@ while ans:
         sys.exit()
     
     elif answers == 1:
-        print "It is certain"
+        print("It is certain")
     
     elif answers == 2:
-        print "Outlook good"
+        print ("Outlook good")
     
     elif answers == 3:
-        print "You may rely on it"
+        print ("You may rely on it")
     
     elif answers == 4:
-        print "Ask again later"
+        print ("Ask again later")
     
     elif answers == 5:
-        print "Concentrate and ask again"
+        print ("Concentrate and ask again")
     
     elif answers == 6:
-        print "Reply hazy, try again"
+        print ("Reply hazy, try again")
     
     elif answers == 7:
-        print "My reply is no"
+        print ("My reply is no")
     
     elif answers == 8:
-        print "My sources say no"
+        print ("My sources say no")
