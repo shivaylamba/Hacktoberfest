3a164cfe5695e543ff7024f28ed771b3c3017fce
Nagendra Singh <Nagendrasingh12471@hotmail.com>
2020-10-10 02:50:56 +0530

    Added String Rotation program in java

---
+++ StringRotation.java
@@ -0,0 +1,19 @@
+// Java String Rotation program
+
+class StringRotation 
+{ 
+    static boolean areRotations(String str1, String str2) 
+    { 
+        return (str1.length() == str2.length()) && 
+               ((str1 + str1).indexOf(str2) != -1); 
+    } 
+    public static void main (String[] args) 
+    { 
+        String str1 = "FGABCDE"; 
+        String str2 = "FGABCDE"; 
+        if (areRotations(str1, str2)) 
+            System.out.println("Strings are rotations of each other"); 
+        else
+            System.out.printf("Strings are not rotations of each other"); 
+    } 
+}
