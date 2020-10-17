b2eeb31b0a2a3e84c9c67c361f0992a3d69b5d18
Vishal Kumar <44905986+virrrex@users.noreply.github.com>
2020-10-17 08:52:37 +0530

    Added hello-blinker.cpp that will keep blinking in terminal

---
+++ hello-blinker.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+    cout<<"Hello World!";
+    system("cls");          //blinking name in terminal
+    main();
+    return 0;
+}
