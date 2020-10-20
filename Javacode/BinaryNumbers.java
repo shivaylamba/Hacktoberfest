58664fe710b105fc7eacd166b91cb1bfd206321e
Mochammad Faisal <mocfaisal712@gmail.com>
2020-10-20 21:42:30 +0700

    Update BinaryNumbers.java

--- Javacode/BinaryNumbers.java
+++ Javacode/BinaryNumbers.java
@@ -1,10 +1,10 @@
-import java.io. * ;
-import java.math. * ;
-import java.security. * ;
-import java.text. * ;
-import java.util. * ;
-import java.util.concurrent. * ;
-import java.util.regex. * ;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class BinaryNumbers {
 
