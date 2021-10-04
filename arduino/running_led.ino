8b64eb2f2f6b6813d9b0cd2d2043ad4607af534f
arincantikam26 <63904430+arincantikam26@users.noreply.github.com>
2021-10-04 14:51:37 +0700

    Update running_led.ino

--- arduino/running_led.ino
+++ arduino/running_led.ino
@@ -1,17 +1,17 @@
 int x[]={1,2,3,4,5,6};
 
 void setup() {
-   for (int=0; i<7; i++){
+   for (int i =0; i<7; i++){
       pinMode(x[i], OUTPUT);
    }	
 }
 
 void loop() {
-   for (int=0; i<7; i++){
+   for (int i=0; i<7; i++){
       digitalWrite(x[i], HIGH);
       delay(100);
    }
-   for (int=6; i>0; i--){
+   for (int i=6; i>0; i--){
       digitalWrite(x[i], LOW);
       delay(50);
    }
