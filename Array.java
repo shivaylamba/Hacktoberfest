1d844072270522e0a899feef5375d2f59d25f6ad
sahilrawat001 <66208562+sahilrawat001@users.noreply.github.com>
2021-10-05 08:15:46 -0700

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,15 @@
+class Array {
+    public static void main(String args[]){
+        int[] marks= {12,23,45,54};//1st method
+       
+        // marks[0]=12;
+        // marks[1 ]=1;  //2nd method
+        // marks[2 ]=182;
+        // marks[3 ]=129;
+          System.out.println(marks[1]);
+         System.out.println(marks.length);
+        //  Array.sort(marks);
+        //  System.out.println(marks[1]);
+
+        }
+}
