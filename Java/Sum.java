f4155d5e8501a76d89fcba3dfda565e83e1cba11
Omkar kulkarni <88308267+Omkar0114@users.noreply.github.com>
2021-10-12 23:05:46 +0530

    Create Sum.java

---
+++ Java/Sum.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num1= 5;
+        int num2= 5;
+
+        int sum= num1 +num2;
+
+        System.out.println("sum is = " + sum );
+    }
+}
