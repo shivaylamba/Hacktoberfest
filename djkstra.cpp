cfb9350d515a9ef6af175aa3f02c475f2bf9909b
rudraskhkarpe <rudraksh.karpe.cs@ghrcem.raisoni.net>
2021-10-15 20:28:03 +0530

    Modifid djkstra.cpp

--- djkstra.cpp
+++ djkstra.cpp
@@ -1,37 +1,57 @@
+// The program is for adjacency matrix representation of the graph
 #include <limits.h>
 #include <stdio.h>
+
+// Number of vertices in the graph
 #define V 9
+
+// A utility function to find the vertex with minimum distance value, from
+// the set of vertices not yet included in shortest path tree
 int minDistance(int dist[], bool sptSet[])
 {
+    // Initialize min value
     int min = INT_MAX, min_index;
     for (int v = 0; v < V; v++)
         if (sptSet[v] == false && dist[v] <= min)
             min = dist[v], min_index = v;
     return min_index;
 }
+// A utility function to print the constructed distance array
 void printSolution(int dist[])
 {
     printf("Vertex \t\t Distance from Source\n");
     for (int i = 0; i < V; i++)
         printf("%d \t\t %d\n", i, dist[i]);
 }
+// Function that implements Dijkstra's single source shortest path algorithm
+// for a graph represented using adjacency matrix representation
 void dijkstra(int graph[V][V], int src)
 {
-    int dist[V];
-    bool sptSet[V];
+    int dist[V]; // The output array.  dist[i] will hold the shortest
+    // distance from src to i
+
+    bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
+    // path tree or shortest distance from src to i is finalized
+
+    // Initialize all distances as INFINITE and stpSet[] as false
     for (int i = 0; i < V; i++)
         dist[i] = INT_MAX, sptSet[i] = false;
+    // Distance of source vertex from itself is always 0
     dist[src] = 0;
     for (int count = 0; count < V - 1; count++)
     {
         int u = minDistance(dist, sptSet);
         sptSet[u] = true;
         for (int v = 0; v < V; v++)
+            // Update dist[v] only if is not in sptSet, there is an edge from
+            // u to v, and total weight of path from src to  v through u is
+            // smaller than current value of dist[v]
             if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
                 dist[v] = dist[u] + graph[u][v];
     }
     printSolution(dist);
 }
+
 int main()
 {
     int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
