eefe5538e0bcb0f546703be553073876ea58fbca
Syed Husnain Haider Bukhari <65342860+revolutionarybukhari@users.noreply.github.com>
2021-10-20 12:15:37 +0500

    wave sort_Monesh.cpp
    Edited for easy output

--- C++/wave sort_Monesh.cpp
+++ C++/wave sort_Monesh.cpp
@@ -29,6 +29,13 @@ int main()
 {
    int arr[] = {1,3,4,7,5,6,2};
    wavesort(arr,7);
+   cout<<"Unsorted:"<<endl;
+	 for(int i=0;i<7; i++)
+   {
+   	 cout<<arr[i]<<" ";
+	   }	
+	cout<<"Sorted:"<<endl;
+   
    
    for(int i=0;i<7; i++)
    {
