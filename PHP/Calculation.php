d39a8145879f4c820e63233a78dddb0e4d69fa80
Simon Krull <simondotunix@gmail.com>
2021-10-27 08:50:10 +0200

    TASK: Add php calculation example

---
+++ PHP/Calculation.php
@@ -0,0 +1,12 @@
+<?php
+
+// First, we define two variables that contain our price
+$pizza = 10;
+$coke = 2.5;
+
+// Now we create a variable that calculates the total price. Here in the case we use the '+' operator
+$totalprice = $pizza + $coke;
+
+// Now we output in a string a text, and the variable $totalprice
+// Output should be: For my pizza and coke together I pay 12,5 dollar
+echo "For my pizza and coke together I pay $totalprice dollar";
