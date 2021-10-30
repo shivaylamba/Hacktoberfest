efaa67e54312001ef7c927e30926ad8d8307d910
Aman sharma <aman05382@gmail.com>
2021-10-30 18:20:23 +0530

    Create Sum_of_Digits.php

---
+++ PHP/Sum_of_Digits.php
@@ -0,0 +1,11 @@
+<?php  
+$num = 14597;  
+$sum=0; $rem=0;  
+  for ($i =0; $i<=strlen($num);$i++)  
+ {  
+  $rem=$num%10;  
+   $sum = $sum + $rem;  
+   $num=$num/10;  
+  }  
+ echo "Sum of digits 14597 is $sum";  
+ ?> 
