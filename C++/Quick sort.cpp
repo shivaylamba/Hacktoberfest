364110ca23eba2af718ae322688400a9eddf52de
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2021-10-31 23:21:12 +0530

    Create Quick sort.cpp

---
+++ C++/Quick sort.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+using  namespace std;
+
+void swap(int arr[],int i,int j){
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+    
+}
+int partition(int arr[],int l,int r){
+    int pivot=arr[r];
+    int i=l-1;
+    for(int j=l;j<r;j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr,i,j);
+        }
+        
+    }
+    swap(arr,i+1,r);
+    return i+1;
+}
+
+void quickSort(int arr[],int l,int r){
+    
+    if(l<r){
+        int pi=partition(arr,l,r);
+        quickSort(arr,l,pi-1);
+        quickSort(arr,pi+1,r);
+        
+    }
+    
+}
+
+int main() {
+    int arr[]={1,77,4,99,66,85,9};
+    quickSort(arr,0,6);
+    for(int i=0;i<7;i++){
+        cout<<arr[i]<< ' ';
+    }cout<<endl;
+    
+    return 0;
+}
