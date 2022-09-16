8fd25f970e13377396f1b520cf8da1d4f6b6747c
Emmanuel Onuorah <emmanuelonuorah875@gmail.com>
2022-09-16 19:57:35 +0100

    Added a greet.js file

---
+++ Greet.js
@@ -0,0 +1,11 @@
+function greet(){
+ const developer = true;
+ if(developer){
+    console.log('Keep making the world a better place one code at a time');
+ }
+ else{
+    console.log('keep being you')
+ }
+}
+
+greet();
