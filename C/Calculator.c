8c1d6093e330f8c36b6c3652b6b1b96fb11f5968
Hardik Sharma <sharmahardikaz@gmail.com>
2022-10-02 23:05:30 +0530

    Updated: Improved User Input Process

--- C/Calculator.c
+++ C/Calculator.c
@@ -2,39 +2,41 @@
 int main()
 {
 	int n1,n2,o,s,a;
-	printf("ENTER 2 NO. =");
-	scanf(" %d %d",&n1,&n2);
-		printf("ENTER operator =");
+	printf("ENTER a number \n");
+	scanf("%d",&n1);
+		printf("ENTER another number \n");
+scanf("%d",&n2);
+printf("ENTER the operator of the operation you want \n");
 	scanf(" %c",&o);
 	
 	switch(o)
 	{
 	case '+':
-		printf("Addition value = %d",n1+n2);
+		printf("Addition result is = %d",n1+n2);
 		break;
 	case '-':
 		if(n1>n2)
 		{
-			printf("Substraction value = %d",n1-n2);
+			printf("Substraction result is = %d",n1-n2);
 			break;
 		}
 			else
 			{
-				printf("Substraction value = %d",n2-n1);
+				printf("Substraction result is= %d",n2-n1);
 			break;
 		}
 	case '*':
-			printf("Multiplication value = %d",n1*n2);
+			printf("Multiplication result is = %d",n1*n2);
 			break;
 	case '/':
 			if(n1>n2)
 			{
-			printf("Substraction value = %d",n1/n2);
+			printf("Division result is = %d",n1/n2);
 			break;
 		}
 			else
 			{
-				printf("Substraction value = %d",n2/n1);
+				printf("Division result is = %d",n2/n1);
 			break;
 		}
 }
