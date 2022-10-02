c5a2bd26cdd1d50815d94c39cbffdcb9ca0dabd7
Drishti Rana <81031606+drish08@users.noreply.github.com>
2022-10-02 14:06:39 +0530

    Create Rock-paper-scissors.py

---
+++ Python/ML Algorithms/Rock-paper-scissors.py
@@ -0,0 +1,51 @@
+import random 
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!")
+else:
+  print(game_images[user_choice])
+
+  computer_choice = random.randint(0, 2)
+  print("Computer chose:")
+  print (game_images[computer_choice])
+
+  
+if user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
