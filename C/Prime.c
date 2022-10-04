830c708a72a31fa0ad4f969d91bcdce2cae36d1e
Shubham Gaur <94301988+404shubham@users.noreply.github.com>
2022-10-04 12:34:47 +0530

    update the prime number in C
    changed the code for finding the first n prime numbers in C.

--- C/Prime.c
+++ C/Prime.c
@@ -1,17 +1,10 @@
-/*       
-          Name: Mura3132
-          Program Name : Print first n Prime Numbers                           
-*/
-
-/*###########################Program start################################## */
-
 #include<stdio.h>
 
 int main()
 {
     int n,i = 3, count, c;
 
-    printf("\nEnter the number of prime numbers required :  ");
+    printf("\number of prime numbers required");
     scanf("%d", &n);
 
     if(n >= 1)
@@ -19,12 +12,9 @@ int main()
         printf("\n\nFirst %d prime numbers are :  ", n);
         printf("2 ");
     }
-
-    // iteration for n prime numbers
-    // i is the number to be checked in each iteration starting from 3
+          
     for(count = 2; count <= n; i++)  
     {
-        // iteration to check c is prime or not
         for(c = 2; c < i; c++)
         {
             if(i%c == 0)
@@ -34,10 +24,9 @@ int main()
         if(c == i)  // c is prime
         {
             printf("%d ", i);
-            count++;    // increment the count of prime numbers
+            count++;
         }
 
     }
     return 0;
 }
-/*###########################Program End################################## */
