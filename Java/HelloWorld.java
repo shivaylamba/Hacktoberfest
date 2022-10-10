7e9f1496551bad94df8dc02f877632e05a430571
Deb-cybe <dmjee1331@gmail.com>
2022-10-10 15:48:01 +0530

    Updated HelloWorld.java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("I am learning Java and Open Source")
 	}
 
 }
