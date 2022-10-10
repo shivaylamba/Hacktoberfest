21b067da11693df4b0b7c874fd0726f1a81fbe03
K.Kirtana <71484190+30kirtana@users.noreply.github.com>
2022-10-10 16:45:17 +0530

    Added sorting programs
    includes insertion sort, bubble sort, mergesort, quicksort, randomized quicksort.

---
+++ C/Bubblesort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int bubblesort(int n,int a[n]){
+int i, j, temp;
+for(i=0; i<n-1; i++)
+{
+ for(j=0; j<n-i-1; j++)
+ {
+ if( a[j] > a[j+1])
+ {
+ temp = a[j]; 
+ a[j] = a[j+1];
+  a[j+1] = temp;
+ }}} 
+ printf("\n");
+ for(int i=0;i<n;i++){
+    printf(" %d ",a[i]);
+}
+ 
+}
+int main(){
+int n;
+printf("Enter number of elements in an array:");
+scanf("%d",&n);
+int a[n];
+for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+}
+bubblesort(n,a);
+}
