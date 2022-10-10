3a0079cfc186c40e16e499252abff3e4452fe63f
Riva Parvez <91824948+Spectre2902@users.noreply.github.com>
2022-10-10 12:21:44 +0530

    Create multi_tables.java

---
+++ multi_tables.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+for(int i=1;i<=10;i++)
+     {
+             System.out.println(N+" "+"x"+" "+ i +" = "+(N*i));
+     }
+        bufferedReader.close();
+    }
+}
