2fe9a57685b91c40b5da823de207a1ea2c9f6be4
priyanshgupta0 <77960686+priyanshgupta0@users.noreply.github.com>
2022-10-17 19:09:29 +0530

    Create proxy_filter.py

---
+++ Python/proxy_filter.py
@@ -0,0 +1,14 @@
+links = """
+
+"""
+# Place proxies in """ """ to fileter 
+for BoostUpStation in links.split("\n"):
+  if(BoostUpStation[-4:] == "8080"):
+    print(BoostUpStation)
+
+
+#Sites to find proxies 
+# https://proxyscrape.com/free-proxy-list
+# https://advanced.name/freeproxy
+# https://www.sslproxies.org/
+# https://premproxy.com/list/
