20d36b56ecdd39406a0c4f020f88bbd0b0993cb2
Rachana priyadarshni samal <77429844+rachana2522@users.noreply.github.com>
2022-10-28 16:59:05 +0530

    Create Kth_Largest_Element_in_an_Array.java

---
+++ Kth_Largest_Element_in_an_Array.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        //https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60294/Solution-explained
+        // shuffle nums to avoid n * n
+        shuffle(nums);
+        k = nums.length - k;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            final int j = partition(nums, lo, hi);
+            if(j < k) {
+                lo = j + 1;
+            } else if (j > k) {
+                hi = j - 1;
+            } else {
+                break;
+            }
+        }
+        return nums[k];
+    }
+
+    private int partition(int[] a, int lo, int hi) {
+
+        int i = lo;
+        int j = hi + 1;
+        while(true) {
+            while(i < hi && less(a[++i], a[lo]));
+            while(j > lo && less(a[lo], a[--j]));
+            if(i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private void exch(int[] a, int i, int j) {
+        final int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    private boolean less(int v, int w) {
+        return v < w;
+    }
+
+    private void shuffle(int a[]) {
+        final Random random = new Random();
+        for(int ind = 1; ind < a.length; ind++) {
+            final int r = random.nextInt(ind + 1);
+            exch(a, ind, r);
+        }
+    }
+}
