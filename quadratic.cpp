40bed918485c91bf44276aa54e79517bc5f9b1c6
Deependra Kumar <deependrakumar15072003@gmail.com>
2023-10-04 17:14:46 +0530

    Update quadratic.cpp
    The code had a logical error in the calculation of the roots of a quadratic equation. Specifically, the formulas used to calculate the roots are incorrect due to missing parentheses. So I have corrected that.
    
    This commit is meant for the purpose of hacktoberfest, please do accept the PR  with hacktoberfest tag on it.

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,26 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-	int a,b,c;
-	cout<<"enter the coefficient of x^2 \t";
-	cin>>a;
-	cout<<"enter the coefficient of x \t ";
-	cin>>b;
-	cout<<"enter the constant ";
-	cin>>c;
-	float dis=(b*b-4*a*c);
-	//cout<<dis;
-	float root1=0,root2=0;
-	if (dis<0)
-		cout<<"no roots exists";
-	else
-	{
-		root1=(-b)/(2.0*a) +sqrt(dis)/2.0*a;
-		root2=(-b)/(2.0*a) -sqrt(dis)/(2.0*a);
-	
-	cout<<"first root is"<<root1;
-	cout<<"second root is"<<root2;
-	}
-	return 0;
+    int a, b, c;
+    cout << "Enter the coefficient of x^2: ";
+    cin >> a;
+    cout << "Enter the coefficient of x: ";
+    cin >> b;
+    cout << "Enter the constant: ";
+    cin >> c;
+
+    float dis = (b * b - 4 * a * c);
+    float root1 = 0, root2 = 0;
+
+    if (dis < 0)
+    {
+        cout << "No roots exist";
+    }
+    else
+    {
+        root1 = (-b + sqrt(dis)) / (2.0 * a);
+        root2 = (-b - sqrt(dis)) / (2.0 * a);
+
+        cout << "First root is " << root1 << endl;
+        cout << "Second root is " << root2 << endl;
+    }
+    return 0;
 }
