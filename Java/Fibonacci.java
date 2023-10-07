626b1850d740b090e08f4c38e64f2cb3bcd65e28
Shubham Kumar <72144322+shubhamraj01@users.noreply.github.com>
2023-10-07 11:10:33 +0530

    Update Fibonacci.java

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -5,11 +5,11 @@ class Fibonacci
         int num1=0, num2=1, num3, i, count=10 ;
         System.out.print(num1 + " " +num2); // i=0 and i=1 i.e. num1 and num2 will be printed 
 
-        for(i=2; i<count; ++i)  //loop starts
+        for(int i=2; i<count; ++i)  //loop starts
         {
            num3 = num1 + num2;
            System.out.print(" " + num3);
-           num1= num2;
+           num1 = num2;
            num2 = num3;
         }
     }
