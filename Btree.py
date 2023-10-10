221664cb93fca7dbe51e9275b637fed7bf820a43
pranav514 <76992202+pranav514@users.noreply.github.com>
2023-10-10 09:21:29 +0530

    Create Btree.py

---
+++ Btree.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val)
+        inorder_traversal(root.right)
+
+# Example usage:
+if __name__ == "__main__":
+    r = Node(50)
+    r = insert(r, 30)
+    r = insert(r, 20)
+    r = insert(r, 40)
+    r = insert(r, 70)
+    r = insert(r, 60)
+    r = insert(r, 80)
+    
+    print("Inorder traversal:")
+    inorder_traversal(r)
