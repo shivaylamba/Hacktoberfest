8fda18fe41dc18d34a9642ed0c64302d18fe643e
Jai Yadav <jaiyadav8875@gmail.com>
2023-10-11 17:50:22 +0530

    Update PrimeOrNot.cpp
    I have added some if conditions to check wether no is prime or not
    ,which reduces the no of iterations significantly as if the number is divisible by 2 or 3 then it is not prime ,from point of view of Time Complexity

--- C++/PrimeOrNot.cpp
+++ C++/PrimeOrNot.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main(){
 int n;
-cin>>n;
+cin>>n;        //input number
+if(n==1)
+    cout<<"Non Prime No."<<endl;
+if(n==2||n==3)
+     cout<<"Non Prime No."<<endl;
+if(n%2==0||n%3==0)
+    cout<<"Non Prime No."<<endl;
 bool flag=0;
 
 for(int i=2;i<=sqrt(n);i++){
@@ -12,11 +18,9 @@ for(int i=2;i<=sqrt(n);i++){
         cout<<"Non Prime No.";
         flag=1;
         break;
-    }
+    }                             // if the no have more factor other than 1 and itself
 }
-if(flag==0){
+if(flag==0)
     cout<<"Prime No."<<endl;
-}
-
     return 0;
 }
