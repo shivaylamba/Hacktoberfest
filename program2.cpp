b716ac17a77d06135c5cfe185a783069ba61d339
Amit0042 <91377510+Amit0042@users.noreply.github.com>
2023-10-21 16:20:27 +0530

    Add files via upload
    Added a new program

---
+++ program2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a ,b,c,i,n;
+    for(i=0;i<n;i++)
+    {
+    cout<<"no. of times ";
+    cin>>n;
+    cout<<"enter values of a and b \n";
+    cin>>a;
+    cin>>b;
+    c = a + b;
+    cout<<c;
+    return 0;
+    }
+}
