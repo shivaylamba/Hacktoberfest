d6c185506661a9bbc68a6a5abc6a25a8eb7e7dce
R Shenoy <rashenoy77@gmail.com>
2023-10-24 14:42:06 +0530

    updated HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Welcome to Hactoberfest 2023");
+		System.out.println("Enjoy Contributing");
 	}
 
 }
